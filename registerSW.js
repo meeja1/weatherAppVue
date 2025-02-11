d2080094c984e857b1178addb055b0b517b7f479
meeja1 <kapustinvovik@gmail.com>
2025-02-11 22:00:23 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://meeja1.github.io/weatherAppVue/sw.js', { scope: 'https://meeja1.github.io/weatherAppVue/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
